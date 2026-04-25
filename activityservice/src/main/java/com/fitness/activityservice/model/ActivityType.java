93de7db24dfe41fc71bf1c2ea3fa7d5de62395cf
Vaishnavi <vaishnavidhanavade9@example.com>
2026-04-25 23:19:36 +0530

    Securityconfig added nd pom.xml changes

---
+++ activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
@@ -0,0 +1,18 @@
+package com.fitness.activityservice.model;
+
+
+
+
+public enum ActivityType {
+
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    WEIGHT_TRAINING,
+    YOGA,
+    HIIT,
+    CARDIO,
+    STRETCHING,
+    OTHER
+}
